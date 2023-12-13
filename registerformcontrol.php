3efcb045416d005cfd54d9e5df70a58502b87435
umarfarookh <sdumarfarookh212@gmail.com>
2023-12-13 22:32:17 +0530

    fixed some sessions

--- registerformcontrol.php
+++ registerformcontrol.php
@@ -11,7 +11,10 @@ $sql= "SELECT * FROM newregister WHERE email= '".$_POST['email']."' ";
 $result= $conn->query($sql);
 $status= "failed";
 if($result->num_rows<0){
-    $_SESSION['email']= $_POST['email'];
+    $_SESSION['name']= $_POST['name'];
+    // $_SESSION['email']= $_POST['email'];
+    // $_SESSION['password']= $_POST['password'];
+    // $_SESSION['mobile']= $_POST['mobile'];
     $sql= "INSERT INTO newregister(name, email, mobile, password) VALUES
 ('".$_POST['name']."','".$_POST['email']."', '".$_POST['mobile']."', '".$_POST['password']."')";
 
@@ -19,10 +22,10 @@ $conn->query($sql);
 $status= "success";
 }
 $conn->close();
-// if($status== 'success'){
-//     header('location:http://google.com/');exit;
-// }
-// else{
-//     header("location:#");exit;
-// }
+if($status== 'success'){
+    header("location:dashboardform.php&$status");exit;
+}
+else{
+    header("location:loginform.php");exit;
+}
 ?>
