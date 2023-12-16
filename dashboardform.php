2db5232556faa309ab7175f84cb66c5c1ecec342
umarfarookh <sdumarfarookh212@gmail.com>
2023-12-16 13:22:17 +0530

    fixed Minor bugs

--- dashboardform.php
+++ dashboardform.php
@@ -18,13 +18,14 @@ else if(!empty($_SESSION['email'])){
 </head>
 <body>
     <div class="nav-header">
-        <p>Company Name</p>
-        <p >Dashboard</p>
-        <input type="text" id="search"  placeholder="Search">
-        <button type="submit" class="submitBtn"><i class="fa-solid fa-magnifying-glass"></i></button>
-        <p></p><i>Good Morning</i> <?php echo $_SESSION['email']?></p>
+        <p>E-commerce</p>
+        <p>Dashboard</p>
         <div class="nav-search">
-        </div>
+            <input type="text" id="search"  placeholder="Search">
+            <!-- <button type="submit" class="submitBtn"><i class="fa-solid fa-magnifying-glass"></i></button> -->
+        </div>  
+        <p><B>Good Morning</B></p>
+        <p><?php echo $_SESSION['email']?></p>
     </div>
     <div class="body">
         <div class="navbar">
@@ -35,8 +36,51 @@ else if(!empty($_SESSION['email'])){
             <a href="logoutform.php">Logout</a>
         </div>
         <div class="body-main">
-            <div class="graph"  style="background-image: url('pictures/graph.jpg') ;"></div>
+            <div class="graph">
+                <div class="img-graph"></div>
+            </div>
             <div class="product">
+                <p>Top Product</p>
+                <a href="https://in.nothing.tech/pages/phone-2?variant=42274864726189" target="blank" alt= "Nothing phone 2">
+                <div class="img-product">
+                    </div>
+                </a>
+                </div>
+            <div class="resultbar" style="overflow-x: auto">
+                <h4>Recent Order</h4>
+                <table>
+                    <tr>
+                        <th>Product</th>
+                        <th>Product ID</th>
+                        <th>Customer</th>
+                        <th>Price</th>
+                    </tr>
+                    <tr>
+                        <td>Nothing phone2</td>
+                        <td>#663142</td>
+                        <td>John Larry</td>
+                        <td>$500</td>
+                    </tr>
+                    <tr>
+                        <td>Samsung s22</td>
+                        <td>#663132</td>
+                        <td>Smith</td>
+                        <td>$800</td>
+                    </tr>
+                    <tr>
+                        <td>Oppo A78</td>
+                        <td>#663149</td>
+                        <td>Andrew</td>
+                        <td>$300</td>
+                    </tr>
+                    <tr>
+                        <td>Apple Iphone X</td>
+                        <td>#663117</td>
+                        <td>Buttler</td>
+                        <td>$900</td>
+                    </tr>
+                </table>
+
             </div>
         </div>
     </div>
